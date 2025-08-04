2fcdf29c2017dc7f177fd2071013130dbf35eb4d
Muhammad-Insha-Khan <164833100+Muhammad-Insha-Khan@users.noreply.github.com>
2025-08-03 18:31:29 -0700

    Update Sign.js

--- frontend/creativeconnects/src/components/Sign.js
+++ frontend/creativeconnects/src/components/Sign.js
@@ -18,7 +18,7 @@ const Signin = () => {
       userType === 'seller'
         ? 'https://creativeconnects.inshakhanbhai78.repl.co/api/seller/signin'
         : userType === 'buyer'
-          ? 'http://localhost:5000/api/buyer/signin'
+          ? 'https://cece364f-9505-4ba8-bfc2-5f7c78a75f2b-00-3mv3m5iuz7ehw.sisko.replit.dev/api/buyer/signin'
           : 'http://localhost:5000/api/skillSwapper/signin'; // SkillSwapper sign-in URL
 
     try {
