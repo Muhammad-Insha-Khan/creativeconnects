2989421d2c7159cc2b249ec0bc719e9e76b83096
Muhammad-Insha-Khan <164833100+Muhammad-Insha-Khan@users.noreply.github.com>
2025-08-05 11:56:58 -0700

    Update AllProjectProposals.js

--- frontend/creativeconnects/src/components/AllProjectProposals.js
+++ frontend/creativeconnects/src/components/AllProjectProposals.js
@@ -25,7 +25,7 @@ const AllProjectProposals = () => {
   useEffect(() => {
     const token = localStorage.getItem('token');
     axios
-      .get('https://fdd95903-fa27-4990-89e9-22e66a027c97-00-32oh6wtdcgz1y.pike.replit.dev/api/seller/signin/api/buyer/all-project-proposals', {
+      .get('https://fdd95903-fa27-4990-89e9-22e66a027c97-00-32oh6wtdcgz1y.pike.replit.dev/api/buyer/all-project-proposals', {
         headers: { Authorization: `Bearer ${token}` }
       })
       .then(res => {
@@ -44,7 +44,7 @@ const AllProjectProposals = () => {
     if (sentProposals.includes(project._id)) return;
 
     try {
-      await axios.post('http://localhost:5000/api/request/project-request', {
+      await axios.post('https://fdd95903-fa27-4990-89e9-22e66a027c97-00-32oh6wtdcgz1y.pike.replit.dev/api/request/project-request', {
         projectId: project._id,
         buyerId: project.buyerId,
         sellerMessage: message,
