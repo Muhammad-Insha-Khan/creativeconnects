50cec8f1527b44cc91429ca47d1d4bbb284ee78b
Muhammad-Insha-Khan <164833100+Muhammad-Insha-Khan@users.noreply.github.com>
2025-08-05 12:23:32 -0700

    Update SkillSwapperProfileEdit.js

--- frontend/creativeconnects/src/components/SkillSwapperProfileEdit.js
+++ frontend/creativeconnects/src/components/SkillSwapperProfileEdit.js
@@ -20,7 +20,7 @@ const SkillSwapperProfileEdit = () => {
 
     const fetchProfile = async () => {
       try {
-        const res = await fetch('http://localhost:5000/api/skillswapper/profile', {
+        const res = await fetch('https://fdd95903-fa27-4990-89e9-22e66a027c97-00-32oh6wtdcgz1y.pike.replit.dev/api/skillswapper/profile', {
           method: 'GET',
           headers: {
             Authorization: `Bearer ${token}`,
