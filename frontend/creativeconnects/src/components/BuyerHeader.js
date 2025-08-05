be22df1158e7d033dcfe4728e9ccbbb61278bed2
Muhammad-Insha-Khan <164833100+Muhammad-Insha-Khan@users.noreply.github.com>
2025-08-05 12:04:02 -0700

    Update BuyerHeader.js

--- frontend/creativeconnects/src/components/BuyerHeader.js
+++ frontend/creativeconnects/src/components/BuyerHeader.js
@@ -61,7 +61,7 @@ const BuyerHeader = () => {
     const token = localStorage.getItem('token');
 
     const deleteEndpoints = [
-      'http://localhost:5000/api/buyer/delete',
+      'https://fdd95903-fa27-4990-89e9-22e66a027c97-00-32oh6wtdcgz1y.pike.replit.dev/api/buyer/delete',
       // adjust endpoint if named differently
     ];
 
