533314cbefc63e25edaa362e9016c377b88dca5a
Muhammad-Insha-Khan <164833100+Muhammad-Insha-Khan@users.noreply.github.com>
2025-08-05 12:11:06 -0700

    Update Notification.js

--- frontend/creativeconnects/src/components/Notification.js
+++ frontend/creativeconnects/src/components/Notification.js
@@ -14,7 +14,7 @@ const Notification = () => {
 
     try {
       await axios.delete(
-        `http://localhost:5000/api/buyer/project-requests/${buyerId}/${requestId}`,
+        `https://fdd95903-fa27-4990-89e9-22e66a027c97-00-32oh6wtdcgz1y.pike.replit.dev/api/buyer/project-requests/${buyerId}/${requestId}`,
         {
           headers: { Authorization: `Bearer ${token}` }
         }
